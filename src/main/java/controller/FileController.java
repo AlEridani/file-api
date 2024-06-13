b7e5a303bf30f0b53934f9542a91c738faa5ed8b
AlEridani@gmail.com <AlEridani@gmail.com>
2024-06-13 20:55:34 +0900

    파일 업로드 다운로드

--- src/main/java/controller/FileController.java
+++ src/main/java/controller/FileController.java
@@ -1,4 +1,46 @@
 package controller;
 
+import org.springframework.core.io.Resource;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import service.FileService;
+
+import java.util.List;
+
+@RequestMapping("/files")
+@RestController
 public class FileController {
+    private final FileService fileService;
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam("files") MultipartFile[] files) {
+        for (MultipartFile file : files) {
+            fileService.saveFile(file);
+        }
+        return ResponseEntity.ok("파일 업로드 성공");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<String>> getFiles() {
+        List<String> fileList = fileService.listFiles();
+        return ResponseEntity.ok(fileList);
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        Resource resource = fileService.loadFile(filename);
+        return ResponseEntity.ok()
+                .header("Content-Disposition", "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
+    }
+
+    @DeleteMapping("/delete/{filename}")
+    public ResponseEntity<String> deleteFile(@PathVariable String filename) {
+        fileService.deleteFile(filename);
+        return ResponseEntity.ok("파일 삭제 성공");
+    }
 }
